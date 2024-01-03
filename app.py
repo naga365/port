0464f0f3ec7ef71e131cee94bd10dbb5809902d6
naga365 <nagagopi33859@gmail.com>
2024-01-03 22:46:55 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,24 @@
+
+from flask import Flask, render_template,request
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/indexdata',methods=['post'])
+def indexdata():
+    Name=request.form['Name']
+    # password=request.form['password']
+    Email=request.form['Email']
+    Subject=request.form['Subject']
+    Message=request.form['Message']
+
+    print(Name,Email,Subject,Message)
+
+    return render_template('index.html',res="sent successfully")
+
+
+if __name__=="__main__":
+    app.run(host='127.0.0.1',port=5001,debug=True)
